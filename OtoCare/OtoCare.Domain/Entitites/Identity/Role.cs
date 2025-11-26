599d579e6560686a39a74045d3d4cb4813a58fbb
BERTAN <163691577+Northerner38@users.noreply.github.com>
2025-11-27 00:04:06 +0300

    Add files via upload

---
+++ OtoCare/OtoCare.Domain/Entitites/Identity/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtoCare.Domain.Entitites.Identity
+{
+    internal class Role
+    {
+    }
+}
---
+++ OtoCare/OtoCare.Domain/Entitites/Identity/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtoCare.Domain.Entities 
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        // "string.Empty" atayarak "null" uyarısı almayı engelliyoruz.
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string? Email { get; set; } // ? koyman güzel, e-posta zorunlu olmayabilir
+
+        // Rol İlişkisi
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!; // Role nesnesi veritabanından dolacak
+
+        // Personel İlişkisi (Opsiyonel olabilir, çünkü her kullanıcı personel olmayabilir)
+        public int? EmployeeId { get; set; }
+        public Employee? EmployeeProfile { get; set; }
+    }
+}
